c734bb7d7154eb1d4510b07b422e55e16990c002
aash29@gmail.com <aash29@gmail.com>
2011-02-18 20:08:46 +0300

    map

---
+++ ElementMap.hpp
@@ -0,0 +1,39 @@
+
+#ifndef ELEMENTMAP_HPP
+#define ELEMENTMAP_HPP
+
+#include <Rocket/Core/Element.h>
+#include <Rocket/Core/EventListener.h>
+#include <Rocket/Core/Types.h>
+
+
+class Map;
+
+/**
+	@author Peter Curry
+ */
+
+class ElementMap : public Rocket::Core::Element, public Rocket::Core::EventListener
+{
+public:
+	ElementMap(const Rocket::Core::String& tag);
+	virtual ~ElementMap();
+
+	/// Intercepts and handles key events.
+	void ProcessEvent(Rocket::Core::Event& event);
+	/// Receive notifications when child elements are added
+	/// This will only get called when we're added to the tree,
+	/// which allows us to bind to onload
+	void OnChildAdd(Rocket::Core::Element* element);
+
+protected:
+	/// Updates the Map.
+	virtual void OnUpdate();
+	/// Renders the Map.
+	virtual void OnRender();
+
+private:
+	Map* map;
+};
+
+#endif
---
+++ map.hpp
@@ -0,0 +1,47 @@
+
+#ifndef MAP_HPP
+#define MAP_HPP
+
+#include <Rocket/Core/Types.h>
+#include <Rocket/Core/Texture.h>
+
+#include <ShellOpenGL.h>
+
+#include <GL/glu.h>
+
+
+
+class Map
+{
+public:
+	Map();
+	~Map();
+
+	/// Initialise a new game
+	void Initialise();
+
+	/// Update the game
+	void Update();
+
+	/// Render the game
+	void Render();
+
+private:
+	// Texture that contains the sprites
+	Rocket::Core::TextureHandle texture;
+
+public:
+	int left;
+	int top;
+	int width;
+	int height;
+
+	GLint viewport[4];
+    GLdouble modelview[16];
+    GLdouble projection[16];
+
+
+
+};
+
+#endif
