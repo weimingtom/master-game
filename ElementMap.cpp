4a0a531ffade5e2a40158670aa94e7828e6ea649
aash29 <aash29@gmail.com>
2011-02-18 20:08:46 +0300

    map

---
+++ ElementMap.cpp
@@ -0,0 +1,104 @@
+
+#undef __WIN32__
+#undef _WIN32
+
+
+#include "ElementMap.hpp"
+#include <Rocket/Core/ElementDocument.h>
+#include <Rocket/Core/Input.h>
+
+#include <sstream>
+#include "map.hpp"
+
+#include <ShellOpenGL.h>
+
+#include <GL/glu.h>
+
+
+ElementMap::ElementMap(const Rocket::Core::String& tag) : Rocket::Core::Element(tag)
+{
+	map = new Map();
+}
+
+ElementMap::~ElementMap()
+{
+	delete map;
+}
+
+// Intercepts and handles key events.
+void ElementMap::ProcessEvent(Rocket::Core::Event& event)
+{
+    Rocket::Core::Element::ProcessEvent(event);
+
+    if (event=="mousemove")
+    {
+        Rocket::Core::Element* dest_element = event.GetTargetElement();
+
+        Rocket::Core::ElementDocument* document  = dest_element->GetOwnerDocument();
+
+        Rocket::Core::Element* coords = document->GetElementById("coords");
+
+        int xcoord = event.GetParameter("mouse_x",0);
+        int ycoord = event.GetParameter("mouse_y",0);
+
+
+
+    GLdouble posX,posY,posZ;
+
+
+    gluUnProject(xcoord,-ycoord + map->viewport[1] + map->viewport[3] +map->top,0,map->modelview,map->projection,map->viewport,&posX,&posY,&posZ);
+
+
+
+        std::string s;
+        std::stringstream out;
+        out << posX << "," <<posY;
+        s = out.str();
+        Rocket::Core::String rs = Rocket::Core::String(s.c_str());
+
+        Rocket::Core::Element* newText = document->CreateElement("p");
+        newText->SetId("pc");
+
+        coords->ReplaceChild((Rocket::Core::Element*)document->CreateTextNode(s.c_str()),coords->GetChild(0));
+
+        //dest_element->Update();
+    }
+
+}
+
+
+// Updates the game.
+void ElementMap::OnUpdate()
+{
+    map->left=GetOwnerDocument()->GetAbsoluteLeft();
+
+    map->top=GetOwnerDocument()->GetAbsoluteTop();
+
+    map->width=GetOwnerDocument()->GetClientWidth();
+
+    map->height=GetOwnerDocument()->GetClientHeight();
+
+	map->Update();
+
+}
+
+// Renders the game.
+void ElementMap::OnRender()
+{
+
+	map->Render();
+
+
+}
+
+void ElementMap::OnChildAdd(Rocket::Core::Element* element)
+{
+	Rocket::Core::Element::OnChildAdd(element);
+
+	if (element == this){
+        GetOwnerDocument()->AddEventListener("mousemove", this);
+        GetOwnerDocument()->AddEventListener("load", this);
+	}
+
+}
+
---
+++ map.cpp
@@ -0,0 +1,88 @@
+
+
+#undef ROCKET_PLATFORM_WIN32
+#undef __WIN32__
+#undef _WIN32
+
+#include "map.hpp"
+#include <Rocket/Core.h>
+#include <Shell.h>
+
+#include <iostream>
+
+// The game's element context (declared in main.cpp).
+extern Rocket::Core::Context* context;
+
+typedef int vertex_tuple[2];
+
+
+Map::Map()
+{
+
+    Rocket::Core::Vector2i texture_dimensions;
+	Rocket::Core::GetRenderInterface()->LoadTexture(texture, texture_dimensions, "assets/invaders.tga");
+
+}
+
+Map::~Map()
+{
+
+}
+
+void Map::Initialise()
+{
+
+}
+
+void Map::Update()
+{
+
+}
+
+void Map::Render()
+{
+   glViewport(left,768 - height - top,width,height);
+
+   glMatrixMode (GL_MODELVIEW);
+
+   glPushMatrix();
+
+    glMatrixMode (GL_PROJECTION);
+    glPushMatrix();
+    glLoadIdentity();
+
+    gluOrtho2D(-200,200,-200,200);
+
+
+    glGetDoublev(GL_MODELVIEW_MATRIX,modelview);
+    glGetDoublev(GL_PROJECTION_MATRIX,projection);
+
+    glGetIntegerv(GL_VIEWPORT,viewport) ;
+
+
+
+   glEnable(GL_TEXTURE_2D);
+
+   	glBindTexture(GL_TEXTURE_2D, (GLuint) texture);
+
+	//glColor4ub(255, 255, 255, 255);
+	glBegin(GL_LINES);
+        glVertex2f(-200.0f,0.0f);
+        glVertex2f(200.0f,0.0f);
+        glVertex2f(0.0f,200.0f);
+        glVertex2f(0.0f,-200.0f);
+
+	glEnd();
+
+	glPopMatrix();
+
+    glMatrixMode (GL_MODELVIEW);
+
+	glPopMatrix();
+
+	glViewport(0,0,1024,768);
+
+}
+
+
+
