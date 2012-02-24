c508fe3aae13f0ef89d981b76688a0fe236c25df
aash29@gmail.com <aash29@gmail.com>
2012-02-24 22:22:02 +0400

    network class

---
+++ objects/components/CompNetworkTemplate.cpp
@@ -0,0 +1,33 @@
+
+#include "CompNetworkTemplate.hpp"
+#include "../../textUtils.hpp"
+#include <cstring>
+#include <vector>
+
+
+
+/*static*/ comp_id_type CompNetworkTemplate::mComponentID = "CompNetwork" ;
+/*static*/ comp_id_type CompNetworkTemplate::mFamilyID = "CompNetwork" ;
+
+
+void CompNetworkTemplate::Deserialize(rapidxml::xml_node<>* node)
+{
+
+    if (node->first_node("pos"))
+    {
+        std::string c2 = node->first_node("pos")->first_attribute("value")->value();
+        std::vector<std::string> pairs = split(c2,',');
+        pos.first=atoi(pairs[0].c_str());
+        pos.second=atoi(pairs[1].c_str());
+    };
+
+    if (node->first_node("passable"))
+    {
+        std::string c2 = node->first_node("passable")->first_attribute("value")->value();
+        std::istringstream (c2)>>passable;
+    };
+
+
+
+};
+
