f06c4c148ed8e82b297fcbbf00ae279ba0cf5b58
aash29 <aash29@gmail.com>
2012-02-24 22:24:09 +0400

    1

--- objects/components/CompHealthTemplate.cpp
+++
@@ -1,5 +0,0 @@
-
-#include "CompHealthTemplate.hpp"
-
-/*static*/ comp_id_type CompHealthTemplate::mComponentID = "CompHealth" ;
-/*static*/ comp_id_type CompHealthTemplate::mFamilyID = "CompHealth" ;
