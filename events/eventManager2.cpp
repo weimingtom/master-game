0ee95d37382360b4ebaf85348d5b0f8746e7368f
KursonSerg <KursonSerg@gmail.com>
2012-06-23 16:43:33 +0300

    fix

--- events/eventManager2.cpp
+++
@@ -1,9 +0,0 @@
-#include "eventManager.hpp"
-
-eventManager *eventManager::mInstance = NULL;
-
-eventManager * eventManager::getInstance() {
-   if(mInstance == NULL)
-	  mInstance = new eventManager();
-   return mInstance;
-}
